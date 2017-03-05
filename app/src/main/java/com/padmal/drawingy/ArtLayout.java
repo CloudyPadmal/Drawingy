9acc267ab51be9e06d1739616f0822d171169063
CloudyPadmal <blog.padmal@gmail.com>
2017-03-05 20:21:18 +0530

    new: Draw paths using circles

--- app/src/main/java/com/padmal/drawingy/ArtLayout.java
+++ app/src/main/java/com/padmal/drawingy/ArtLayout.java
@@ -1,8 +1,16 @@
 package com.padmal.drawingy;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Padmal on 3/5/17.
@@ -10,9 +18,44 @@ import android.view.View;
 
 public class ArtLayout extends View {
 
+    private final int color = Color.BLACK;
+
+    private Paint paint;
+
+    private List<Point> points;
+
     public ArtLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        initiatePaint();
+        points = new ArrayList<>();
     }
 
+    private void initiatePaint() {
+        paint = new Paint();
+        paint.setColor(color);
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(2);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+    }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        for (Point point : points) {
+            canvas.drawCircle(point.x, point.y, 5, paint);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+        points.add(new Point(Math.round(touchX), Math.round(touchY)));
+        // Redraw
+        postInvalidate();
+        return true;
+    }
 }
--- app/src/main/java/com/padmal/drawingy/MainActivity.java
+++ app/src/main/java/com/padmal/drawingy/MainActivity.java
@@ -5,9 +5,18 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ArtLayout artLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initiateView();
+
+    }
+
+    private void initiateView() {
+        artLayout = (ArtLayout) findViewById(R.id.art_layout);
     }
 }
