e62d4589bd14670e6dfe3fbecfaec89243ef8586
CloudyPadmal <blog.padmal@gmail.com>
2017-03-05 20:02:24 +0530

    new: ArtLayout

---
+++ app/src/main/java/com/padmal/drawingy/ArtLayout.java
@@ -0,0 +1,18 @@
+package com.padmal.drawingy;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Padmal on 3/5/17.
+ */
+
+public class ArtLayout extends View {
+
+    public ArtLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
